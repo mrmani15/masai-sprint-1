aa364af8a3551e7f68287aea0876fa97554b2d80
Manish Kumar <mk778281@gmail.com>
2019-11-29 17:49:36 +0530

    sprint-2

---
+++ sprint-2/health.js
@@ -0,0 +1,16 @@
+	
+
+
+	function itemselected(){
+		item_sel = document.getElementById("items").value
+		var item_select = document.createElement("p")
+		item_select.textContent = item_sel
+		document.getElementById("selctedthings").appendChild(item_select)
+	}
+
+	function activityselected(){
+		activity_sel = document.getElementById("acti").value
+		var activity_select = document.createElement("p")
+		activity_select.textContent = activity_sel
+		document.getElementById("selctedactivity").appendChild(activity_select)
+	}
