dd3357a9694854400aee843e7ee309f74aaf36ca
Manish Kumar <mk778281@gmail.com>
2020-02-28 22:53:29 +0530

    import router

--- Superhero/src/components/Result.jsx
+++ Superhero/src/components/Result.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import {Route, BrowserRouter as Router, Link} from "react-router-dom"
 
 class Result extends Component {
     constructor(props) {
