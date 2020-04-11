67c111de78b735d8178145813ef1bee58a11c9b5
Manish Kumar <mk778281@gmail.com>
2020-04-11 22:58:18 +0530

    bug fixes

--- Superhero/src/Redux/Action.js
+++
@@ -1,8 +0,0 @@
-export const ALL_DATA = "ALL_DATA";
-
-export const getDataFromUser = (data) => {
-    return{
-        type :ALL_DATA,
-        data
-    }
-}
--- gamblingtonight/action6.js
+++
@@ -1,6 +0,0 @@
-	display = localStorage.getItem("username");
-	document.getElementById("print_name").innerHTML = "Hello Mr. " + display;
-
-	function main_game(){
-		window.location.href = "sixgame.html";
-	}
